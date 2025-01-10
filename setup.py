4ae6abade35336c821e283d56fd85978e83aa0d5
dexian <dxe.tang@gmail.com>
2025-01-10 23:34:01 +0100

    add dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "docx-python",
+        "python-docx",
         "langchain-text-splitters",
         "python-multipart",
         "faiss-cpu",
